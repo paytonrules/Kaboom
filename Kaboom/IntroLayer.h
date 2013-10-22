ea71a78875c4c82b950872403f3fddbd037b985b
Eric Smith <esmith@8thlight.com>
2013-10-22 08:41:56 -0500

    Initial setup
    A generated cocos2d project, modified to use cocos2d as a cocoapod
    and with OCDSpec and OCMock setup.

---
+++ Kaboom/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Kaboom
+//
+//  Created by Eric Smith on 10/22/13.
+//  Copyright Eric Smith 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '7.0'
+
+pod 'cocos2d'
+
+target :Specs do
+  pod 'OCMock'
+  pod 'OCDSpec2'
+end
+
