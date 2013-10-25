0a968c6505e4b96d3e761c1868d7c1638a53a699
Eric Smith <esmith@8thlight.com>
2013-10-25 09:26:21 -0500

    Display those bombs

--- Kaboom/Cocos/BomberSprite.m
+++ Kaboom/Cocos/BomberSprite.m
@@ -1,6 +1,8 @@
 #import "BomberSprite.h"
 #import "Bomber.h"
 
+const int kBomb = 200;
+
 @interface BomberSprite()
 
 @property(strong) Bomber *bomber;
@@ -21,6 +23,17 @@
 {
   [self.bomber update:delta];
   [self setPosition:self.bomber.position];
+
+  [self.bomber.bombs enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+    [self.parent removeChildByTag:kBomb + idx];
+    NSValue *bombValue = (NSValue *)obj;
+    CGPoint location;
+    [bombValue getValue:&location];
+
+    CCSprite *bombSprite = [CCSprite spriteWithFile:@"bomb.png"];
+    bombSprite.position = location;
+    [self.parent addChild:bombSprite z:0 tag:kBomb + idx];
+  }];
 }
 
 @end
