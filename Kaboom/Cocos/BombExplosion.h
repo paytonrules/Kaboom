0679e12894d28719386395e89345e84aaf50b8f1
Eric Smith <esmith@8thlight.com>
2013-12-27 10:07:30 -0600

    Working explosions.

--- Kaboom/Cocos/BombExplosion.h
+++ Kaboom/Cocos/BombExplosion.h
@@ -2,6 +2,6 @@
 
 @class BombSprite;
 
-@interface BombExplosion : CCParticleSmoke
+@interface BombExplosion : CCParticleSystemQuad
 
 @end
--- Kaboom/Cocos/BombExplosion.m
+++ Kaboom/Cocos/BombExplosion.m
@@ -1,13 +1,35 @@
 #import "BombExplosion.h"
-#import "BombSprite.h"
 #import "KaboomLayer.h"
 
 
 @implementation BombExplosion
 
 - (id)init {
-  self = [super init];
-  if (self) {
+  if (self = [super init]) {
+    self.emitterMode = kCCParticleModeGravity;
+    self.position = ccp(40.00, 20.00);
+    self.emissionRate = 80.0;
+    self.duration = 0.25;
+    self.totalParticles = 250;
+    self.life = 0.15;
+    self.lifeVar = 0.25;
+    self.startSize = 54.00;
+    self.startSizeVar = 10.00;
+    self.endSize = 0.0;
+    self.endSizeVar = 0.0;
+    self.startSpin = 0.0;
+    self.startSpinVar = 0.0;
+    self.endSpin = 0.0;
+    self.endSpinVar = 0.0;
+    self.angle = 90.0;
+    self.angleVar = 10.0;
+    self.startColor = ccc4f(194.0, 64.0, 31.0, 255.0);
+    self.startColorVar = ccc4f(0.0, 0.0, 0.0, 0.0);
+    self.endColor = ccc4f(0.0, 0.0, 0.0, 255.0);
+    self.endColorVar = ccc4f(0.0, 0.0, 0.0, 0.0);
+    [self setBlendFunc: (ccBlendFunc) { GL_ONE, GL_ONE }];
+    self.texture = [[CCTextureCache sharedTextureCache] addImage: @"ccbParticleFire.png"];
+
     self.autoRemoveOnFinish = YES;
   }
 
