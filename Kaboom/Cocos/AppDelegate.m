a9c48dd21aa5ba5e6d7051c5516766948cf6f998
Eric Smith <esmith@8thlight.com>
2014-02-09 12:59:18 -0600

    Sprite Screens working!
    What a hassle.  Maybe there's a better workflow for multiple
    resolutions.

--- Kaboom/Cocos/AppDelegate.m
+++ Kaboom/Cocos/AppDelegate.m
@@ -15,9 +15,6 @@
 
 @implementation MyNavigationController
 
-// The available orientations should be defined in the Info.plist file.
-// And in iOS 6+ only, you can override it in the Root View controller in the "supportedInterfaceOrientations" method.
-// Only valid for iOS 6+. NOT VALID for iOS 4 / 5.
 -(NSUInteger)supportedInterfaceOrientations {
 	
 	// iPhone only
@@ -28,18 +25,6 @@
 	return UIInterfaceOrientationMaskLandscape;
 }
 
-// Supported orientations. Customize it for your own needs
-// Only valid on iOS 4 / 5. NOT VALID for iOS 6.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-	// iPhone only
-	if( [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone )
-		return UIInterfaceOrientationIsLandscape(interfaceOrientation);
-	
-	// iPad only
-	// iPhone only
-	return UIInterfaceOrientationIsLandscape(interfaceOrientation);
-}
 
 // This is needed for iOS4 and iOS5 in order to ensure
 // that the 1st scene has the correct dimensions
