e39ae3523a3483b1c8daa758d0245dc77354fc1b
Eric Smith <esmith@8thlight.com>
2014-04-13 09:54:52 -0500

    Restore kaboom context specs, so I can make more changes

--- Specs/KaboomContextSpec.m
+++ Specs/KaboomContextSpec.m
@@ -1,20 +1,18 @@
-#import <OCDSpec2/OCDSpec2.h>
-#import <OCMock/OCMock.h>
+#import <Kiwi/Kiwi.h>
 #import "KaboomContext.h"
 #import "LevelCollectionArray.h"
 #import "Bomber.h"
 #import "Buckets.h"
 
 // Most of these specs are still in the KaboomSpec
-// Remove the ManualLevelStuff - your current one will already do that
-OCDSpec2Context(KaboomContextSpec) {
+SPEC_BEGIN(KaboomContextSpec)
 
-  Describe(@"Kaboom Context", ^{
+  describe(@"Kaboom Context", ^{
     __block KaboomContext *kaboomContext;
     __block id machine;
 
-    BeforeEach(^{
-      machine = [OCMockObject niceMockForProtocol:@protocol(KaboomStateMachine)];
+    beforeEach(^{
+      machine = [KWMock mockForProtocol:@protocol(KaboomStateMachine)];
       kaboomContext = [KaboomContext newWithMachine:machine];
 
       kaboomContext.levels = [LevelCollectionArray newWithArray:@[@{
@@ -26,141 +24,131 @@ OCDSpec2Context(KaboomContextSpec) {
       }]];
     });
 
-    Describe(@"advancing the levels", ^{
+    describe(@"advancing the levels", ^{
 
-      It(@"sets the levels at the first one on start bombing", ^{
-        id bomber = [OCMockObject niceMockForProtocol:@protocol(Bomber)];
+      it(@"sets the levels at the first one on start bombing", ^{
+        [[machine stub] fire:any()];
+        id bomber = [KWMock mockForProtocol:@protocol(Bomber)];
         kaboomContext.bomber = bomber;
 
         [[bomber expect] startAtSpeed:1 withBombs:1];
 
         [kaboomContext startBombing];
-
-        [bomber verify];
       });
 
-      It(@"resets the games level back to the first level", ^{
-        id bomber = [OCMockObject niceMockForProtocol:@protocol(Bomber)];
-        kaboomContext.bomber = bomber;
-
-        [[bomber expect] startAtSpeed:1 withBombs:1];
-
-        [kaboomContext advanceToNextLevel];
-        [kaboomContext resetGame];
-
-        [[bomber expect] startAtSpeed:1 withBombs:1];
-
-        [kaboomContext startBombing];
-
-        [bomber verify];
-      });
-
-      It(@"moves to the next level when the bomber uses up the bombs", ^{
-        id bomber = [OCMockObject niceMockForProtocol:@protocol(Bomber)];
-        kaboomContext.bomber = bomber;
-
-        [[[bomber stub] andReturnValue:@YES] isOut];
-        [[[bomber stub] andReturnValue:@NO] bombHit];
-
-        [[machine expect] fire:@"Next Level"];
-
-        [kaboomContext updatePlayers:10.0];
-
-        [machine verify];
-      });
-
-      It(@"requests a new level when advancing to the next level", ^{
-        id bomber = [OCMockObject niceMockForProtocol:@protocol(Bomber)];
-        kaboomContext.bomber = bomber;
-
-        [[machine expect] fire:@"New Level"];
-
-        [kaboomContext advanceToNextLevel];
-
-        [machine verify];
-      });
-
-      It(@"starts the bomber at the next level", ^{
-        id bomber = [OCMockObject niceMockForProtocol:@protocol(Bomber)];
+      it(@"resets the games level back to the first level", ^{
+        [[machine stub] fire:any()];
+        id bomber = [KWMock mockForProtocol:@protocol(Bomber)];
         kaboomContext.bomber = bomber;
 
         [[bomber expect] startAtSpeed:2 withBombs:2];
 
         [kaboomContext advanceToNextLevel];
+        
+        [[bomber expect] startAtSpeed:1 withBombs:1];
+        [kaboomContext resetGame];
+      });
 
-        [bomber verify];
+      it(@"moves to the next level when the bomber uses up the bombs", ^{
+        id bomber = [KWMock mockForProtocol:@protocol(Bomber)];
+        kaboomContext.bomber = bomber;
+        
+        [[bomber stub] update:10.0];
+        [[bomber stub] updateDroppedBombs:any()];
+        [[bomber stubAndReturn:theValue(YES)] isOut];
+        [[bomber stubAndReturn:theValue(NO)] bombHit];
+
+        [[machine expect] fire:@"Next Level"];
+
+        [kaboomContext updatePlayers:10.0];
+      });
+
+      it(@"requests a new level when advancing to the next level", ^{
+        [[machine expect] fire:@"New Level"];
+
+        [kaboomContext advanceToNextLevel];
+      });
+
+      it(@"starts the bomber at the next level", ^{
+        [[machine stub] fire:any()];
+        id bomber = [KWMock mockForProtocol:@protocol(Bomber)];
+        kaboomContext.bomber = bomber;
+
+        [[bomber expect] startAtSpeed:2 withBombs:2];
+
+        [kaboomContext advanceToNextLevel];
       });
     });
 
-    Describe(@"updating players", ^{
+    describe(@"updating players", ^{
 
-      It(@"checks for caught buckets after updating their positions", ^{
-        id bomber = [OCMockObject mockForProtocol:@protocol(Bomber)];
+      it(@"checks for caught buckets after updating their positions", ^{
+        [[machine stub] fire:any()];
+        id bomber = [KWMock mockForProtocol:@protocol(Bomber)];
         [[bomber stub] startAtSpeed:0.0 withBombs:0];
-        [[[bomber stub] andReturnValue:@NO] bombHit];
-        [[[bomber stub] andReturnValue:@1] droppedBombCount];
-        [[[bomber stub] andReturnValue:@NO] isOut];
-        id buckets = [OCMockObject mockForClass:[Buckets class]];
-        [[[buckets stub] andReturnValue:@1] bucketCount];
+        [[bomber stubAndReturn:theValue(NO)] bombHit];
+        [[bomber stubAndReturn:theValue(1)] droppedBombCount];
+        [[bomber stubAndReturn:theValue(NO)] isOut];
+        id buckets = [Buckets mock];
+        [[buckets stubAndReturn:theValue(1)] bucketCount];
 
         kaboomContext.bomber = bomber;
         kaboomContext.buckets = buckets;
 
-        [bomber setExpectationOrderMatters:YES];
+   //     [bomber setExpectationOrderMatters:YES];
         [[bomber expect] update:10];
         [[buckets expect] update:10];
 
         [[bomber expect] updateDroppedBombs:buckets];
 
         [kaboomContext updatePlayers:10];
-
-        [bomber verify];
-        [buckets verify];
       });
 
     });
 
-    Describe(@"ending a game", ^{
+    describe(@"ending a game", ^{
 
-      It(@"will fire an end game when the user is out of buckets", ^{
-        id bomber = [OCMockObject niceMockForProtocol:@protocol(Bomber)];
-        id buckets = [OCMockObject mockForClass:[Buckets class]];
+      it(@"will fire an end game when the user is out of buckets", ^{
+        id bomber = [KWMock mockForProtocol:@protocol(Bomber)];
+        id buckets = [Buckets mock];
         kaboomContext.bomber = bomber;
         kaboomContext.buckets = buckets;
 
-        [[[bomber stub] andReturnValue:@YES] bombHit];
+        [[bomber stubAndReturn:theValue(YES)] bombHit];
+        [[bomber stub] update:10];
+        [[bomber stub] updateDroppedBombs:buckets];
+        [[bomber stub] explode];
         [[buckets stub] removeBucket];
         [[buckets stub] update:10.0];
-        [[[buckets stub] andReturnValue:@0] bucketCount];
+        [[buckets stubAndReturn:theValue(0)] bucketCount];
         [[buckets stub] reset];
 
         [[machine expect] fire:@"End Game"];
 
         [kaboomContext updatePlayers:10];
-
-        [machine verify];
       });
     });
 
-    Describe(@"bombs hitting", ^{
+    describe(@"bombs hitting", ^{
 
-      It(@"starts exploding when a bomb hits", ^{
-        id bomber = [OCMockObject niceMockForProtocol:@protocol(Bomber)];
-        [[[bomber stub] andReturnValue:@YES] bombHit];
-        id buckets = [OCMockObject niceMockForClass:[Buckets class]];
-        [[[buckets stub] andReturnValue:@1] bucketCount];
+      it(@"starts exploding when a bomb hits", ^{
+        id buckets = [Buckets mock];
+        [[buckets stubAndReturn:theValue(1)] bucketCount];
+        [[buckets stub] update:1];
+        [[buckets stub] removeBucket];
+        id bomber = [KWMock mockForProtocol:@protocol(Bomber)];
+        [[bomber stubAndReturn:theValue(YES)] bombHit];
+        [[bomber stub] update:1];
+        [[bomber stub] updateDroppedBombs:buckets];
+        [[bomber stub] explode];
         kaboomContext.bomber = bomber;
         kaboomContext.buckets = buckets;
 
         [[machine expect] fire:@"Bomb Hit"];
 
         [kaboomContext updatePlayers:1];
-
-        [machine verify];
       });
-
     });
   });
 
-
-}
+SPEC_END
